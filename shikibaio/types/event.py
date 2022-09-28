8fca7443397c14fa0be082fe910aafba18460d09
ren3104 <2ren3104@gmail.com>
2022-09-28 21:18:05 +0300

    Update event.py

--- shikibaio/types/event.py
+++ shikibaio/types/event.py
@@ -2,25 +2,27 @@ from dataclasses import dataclass
 from typing import Any, Dict, Optional, Union
 
 from shiki4py import Shikimori
+from shiki4py.types import Comment
 
 from shikibaio.enums import DataType, EventType
 
 
 @dataclass
 class Event:
-    data_type: str
+    _api: Shikimori
+    data_type: Union[DataType, str]
     event_type: Union[EventType, str]
     data_id: int
     chat_id: int
     user_id: int
     text: str
-    data: Dict[str, Any]
+    data: Comment
 
     @staticmethod
-    async def create(
-        api: Shikimori, raw: Dict[str, Any], data: Optional[Dict[str, Any]] = None
-    ) -> "Event":
+    async def create(api: Shikimori, raw: Dict[str, Any]) -> "Event":
         kwargs: Dict[str, Any] = {}
+
+        kwargs["_api"] = api
 
         data_type: str
         event_type: str
@@ -37,20 +39,21 @@ class Event:
             kwargs["event_type"] = event_type
 
         if data_type == "comment":
-            if data != None:
-                kwargs["data"] = data
-            else:
-                kwargs["data"] = await api.comments.show_one(raw["data"]["comment_id"])
+            kwargs["data"] = await api.comments.show_one(raw["data"]["comment_id"])
 
-            if isinstance(kwargs["data"], dict):
-                kwargs["data_id"] = kwargs["data"]["id"]
-                kwargs["chat_id"] = kwargs["data"]["commentable_id"]
-                kwargs["user_id"] = kwargs["data"]["user_id"]
-                kwargs["text"] = kwargs["data"]["body"]
-            else:
-                kwargs["data_id"] = kwargs["data"].__getattribute__("id")
-                kwargs["chat_id"] = kwargs["data"].__getattribute__("commentable_id")
-                kwargs["user_id"] = kwargs["data"].__getattribute__("user_id")
-                kwargs["text"] = kwargs["data"].__getattribute__("body")
+            kwargs["data_id"] = kwargs["data"].id
+            kwargs["chat_id"] = kwargs["data"].commentable_id
+            kwargs["user_id"] = kwargs["data"].user_id
+            kwargs["text"] = kwargs["data"].body
 
         return Event(**kwargs)
+
+    async def answer(
+        self,
+        text: str,
+        is_offtopic: Optional[bool] = None,
+        broadcast: Optional[bool] = None,
+    ) -> Comment:
+        return await self._api.comments.create(
+            text, self.chat_id, self.data.commentable_type, is_offtopic, broadcast
+        )
